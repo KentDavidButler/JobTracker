ea51d3be0eac5c00d1630afb6d8bfb0306b684de
kbutler <kbutler@MacBookPro.localdomain>
2025-02-26 15:51:14 -0500

    Get all job postings from db functioning

--- src/Controller/main.go
+++ src/Controller/main.go
@@ -11,7 +11,13 @@ import (
 )
 
 func GetJobPostings(c *gin.Context, db *sql.DB) {
-	c.IndentedJSON(http.StatusOK, model.JobPostings)
+	jobs := model.GetJobPostings(db, 0)
+
+	if len(jobs) > 0 {
+		c.IndentedJSON(http.StatusOK, jobs)
+		return
+	}
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "list is empty"})
 }
 
 func GetJobPostingsByID(c *gin.Context, db *sql.DB) {
@@ -22,7 +28,7 @@ func GetJobPostingsByID(c *gin.Context, db *sql.DB) {
 		c.IndentedJSON(http.StatusOK, job)
 		return
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "job not found"})
 }
 
 func PostJobPosting(c *gin.Context, db *sql.DB) {
--- src/Model/main.go
+++ src/Model/main.go
@@ -25,8 +25,28 @@ var JobPostings = []JobPosting{
 	{ID: "39cb5563-f85a-43d2-a815-51ced1138b9f", CompanyName: "someCompany", PositionLink: "www.somecompany.com"},
 }
 
-func GetJobPostings(db *sql.DB) []JobPosting {
-	return JobPostings
+func GetJobPostings(db *sql.DB, offset int16) []JobPosting {
+	var postings []JobPosting
+
+	rows, err := db.Query(`SELECT * FROM job_postings ORDER BY company_name LIMIT 
+		100 OFFSET $1`, offset)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var job JobPosting
+		err := rows.Scan(&job.ID, &job.CompanyName,
+			&job.ReferralName, &job.ReferralNotes, &job.ApplicationSubmissionDate, &job.PositionLink,
+			&job.GoogleDocLink, &job.Interview, &job.InterviewDate, &job.Denial, &job.AdditionalInfo)
+		if err != nil {
+			log.Fatal(err)
+		}
+		postings = append(postings, job)
+	}
+
+	return postings
 }
 
 func GetJobPostingsByID(db *sql.DB, id string) JobPosting {
